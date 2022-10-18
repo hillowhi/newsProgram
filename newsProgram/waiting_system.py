76528bc11ddc4e8135ccf924d9a228123102ccf0
hh <957511968@qq.com>
2022-10-18 17:52:49 +0800

    ui自动化基础操作记录&练习-等待机制+页面加载机制 🐸爱学习可爱(✿◡‿◡)

--- newsProgram/waiting_system.py
+++ newsProgram/waiting_system.py
@@ -4,10 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 driver = webdriver.Chrome()
 options = webdriver.ChromeOptions
-options.page_load_strategy = "eager"
+options.page_load_strategy = "none"
 # driver.get("https://broker-dev.nihao.com/broker-api/haha/swagger-ui.html#!/234542120826381921153255090001475"
 #            "/deleteVideoUsingGET")
-driver.switch_to.new_window("window")
+# driver.switch_to.new_window("window")
 driver.get("https://image.baidu.com/search/albumsdetail?tn=albumsdetail&word=%E8%88%AA%E6%8B%8D%E5%9C%B0%E7%90%83%E7"
            "%B3%BB%E5%88%97&fr=albumslist&album_tab=%E8%AE%BE%E8%AE%A1%E7%B4%A0%E6%9D%90&album_id=312&rn=30")
 # sleep(2)
